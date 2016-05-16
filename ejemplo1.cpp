40e085c4c7cfa1b1aa5cd32aa7b2b5bc0d0d0198
Gaston11 <amurriog@hotmail.com>
2016-05-16 18:30:15 -0300

    es un ejemplo de algo

---
+++ ejemplo1.cpp
@@ -0,0 +1,21 @@
+//============================================================================
+// Name        : ejemplo1.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+
+	int num =23;
+
+	if (num)
+		cout << "hola" << endl;
+
+	return 0;
+}
